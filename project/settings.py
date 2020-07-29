bee7c8c5ce1e86d2d28ec92433cb715a66a5cd6a
olbed <mr.oo.leg@gmail.com>
2020-07-29 09:30:02 +0300

    manage.py createsuperuser fixed by implementing custom user manager

--- project/settings.py
+++ project/settings.py
@@ -100,8 +100,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'project.wsgi.application'
 
 AUTHENTICATION_BACKENDS = [
-    # Needed to login by username in Django admin, regardless of `allauth`
-    'django.contrib.auth.backends.ModelBackend',
     # `allauth` specific authentication methods, such as login by e-mail
     'allauth.account.auth_backends.AuthenticationBackend',
 ]
